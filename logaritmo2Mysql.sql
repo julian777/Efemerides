89ce9996fe87f5cf47eb93b0c15a5141b7f6bbbf
julian777 <julian01cs@hotmail.com>
2017-06-18 18:32:42 -0600

    Logaritmos Tablas

---
+++ logaritmo2Mysql.sql
@@ -0,0 +1,441 @@
+Use Efemerides
+/* CREATE TABLE */
+CREATE TABLE LogaritmosDos(
+`minuto` INT,
+`3` INT,
+`4` INT,
+`5` INT,
+`6` INT,
+`7` INT,
+`8` INT,
+`9` INT
+);
+
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+0, 90309, 77815, 68124, 60206, 53511,
+47712, 42597
+);
+
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+1, 90068, 77635, 67980, 60086, 53408,
+47622, 42516
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+2, 89829, 77455, 67836, 59956, 53305,
+47532, 42436
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+3, 89591, 77276, 67692, 59846, 53202,
+47442, 42356
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+4, 89354, 77097, 67549, 59726, 53100,
+47352, 42276
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+5, 89119, 76920, 67406, 59607, 52997,
+47262, 42196
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+6, 88885, 76743, 67264, 59488, 52895,
+47173, 42117
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+7, 88652, 76566, 67122, 59370, 52793,
+47083, 42037
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+8, 88420, 76391, 66981, 59251, 52692,
+46994, 41958
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+9, 88190, 76216, 66840, 59133, 52590,
+46905, 41879
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+10, 87961, 76042, 66700, 59016, 52489,
+46817, 41800
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+11, 87733, 75869, 66560, 58899, 52388,
+46728, 41721
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+12, 87506, 75696, 66421, 58782, 52288,
+46640, 41642
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+13, 87281, 75524, 66282, 58656, 52187,
+46552, 41564
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+14, 87056, 75353, 66143, 58549, 52087,
+46464, 41485
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+15, 85833, 75182, 66005, 58433, 51987,
+46376, 41407
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+16, 86611, 75012, 65868, 58317, 51888,
+46288, 41329
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+17, 86390, 74843, 65731, 58202, 51788,
+46200, 41251
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+18, 86170, 74674, 65594, 58087, 51689,
+46113, 41173
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+19, 85951, 74506, 65457, 57972, 51590,
+46026, 41095
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+20, 85733, 74339, 65321, 57858, 51491,
+45939, 41017
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+21, 85516, 74172, 65186, 57744, 51392,
+45852, 40940
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+22, 85301, 74006, 65051, 57630, 51294,
+45766, 40863
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+23, 85087, 73841, 64916, 57516, 51196,
+45679, 40785
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+24, 84873, 73675, 64782, 57403, 51098,
+45593, 40708
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+25, 84661, 73512, 64648, 57290, 51000,
+45507, 40631
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+26, 84450, 73348, 64514, 57178, 50903,
+45421, 40555
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+27, 84239, 73185, 64381, 57066, 50805,
+45335, 40478
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+28, 84030, 73023, 64249, 56953, 50708,
+45250, 40401
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+29, 83822, 72851, 64117, 56841, 50611,
+45164, 40325
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+30, 83614, 72700, 63985, 56730, 50515,
+45079, 40249
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+31, 83408, 72539, 63853, 56619, 50418,
+44994, 40173
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+32, 83203, 72379, 63722, 56508, 50322,
+44909, 40097
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+33, 82998, 72220, 63502, 56397, 50226,
+44824, 40021
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+34, 82795, 72061, 63462, 56287, 50131,
+44740, 39945
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+35, 82502, 71903, 63332, 56177, 50035,
+44655, 39869
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+36, 82391, 71745, 63202, 56067, 49940,
+44571, 39794
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+37, 82191, 71588, 63073, 55957, 49845,
+44487, 39719
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+38, 81991, 71432, 62945, 55848, 49750,
+44403, 39643
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+39, 81792, 71276, 62816, 55739, 49655,
+44319, 39568
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+40, 81594, 71120, 62688, 55630, 49560,
+44236, 39493
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+41, 81397, 70965, 62561, 55522, 49466,
+44152, 39419
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+42, 81201, 70811, 62434, 55414, 49372,
+44089, 39344
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+43, 81006, 70657, 62307, 55306, 49278,
+43986, 39269
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+44, 80811, 70504, 62180, 55198, 49184,
+43903, 39195
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+45, 80618, 70352, 62054, 55091, 49091,
+43820, 39121
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+46, 80425, 70200, 61929, 54984, 48998,
+43738, 39046
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+47, 80234, 70048, 61803, 54877, 48905,
+43656, 38972
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+48, 80043, 69897, 61678, 54770, 48812,
+43573, 38899
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+49, 79853, 69746, 61553, 54664, 48719,
+43491, 38825
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+50, 79663, 69596, 61429, 54558, 48626,
+43409, 38751
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+51, 79475, 69447, 61305, 54452, 48534,
+43327, 38677
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+52, 79287, 69298, 61182, 54347, 48442,
+43245, 38604
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+53, 79100, 69150, 61059, 54241, 48350,
+43163, 38531
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+54, 78915, 69002, 60936, 54136, 48258,
+43092, 38458
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+55, 78730, 68854, 60813, 54031, 48167,
+43001, 38385
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+56, 78545, 68707, 60691, 53927, 48076,
+42920, 38312
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+57, 78361, 68561, 60569, 53823, 47984,
+42839, 38239
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+58, 78179, 68415, 60448, 53719, 47893,
+42758, 38166
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+59, 77997, 68269, 60327, 53615, 47802,
+42677, 38094
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosDos(`minuto`, `3`,`4`, `5`, `6`, `7`, `8`, `9`)
+VALUES
+(
+60, 77815, 68124, 60206, 53511, 47712,
+42597, 38021
+);
