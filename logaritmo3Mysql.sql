edc6898ecb112c88bd2d7428e9c9c9a99418503a
julian777 <julian01cs@hotmail.com>
2017-06-20 21:36:55 -0600

    Logaritmos Tablas 3

---
+++ logaritmo3Mysql.sql
@@ -0,0 +1,438 @@
+/* CREATE TABLE */
+CREATE TABLE IF NOT EXISTS LogaritmosTres(
+`minuto` INT,
+`10` INT,
+`11` INT,
+`12` INT,
+`13` INT,
+`14` INT,
+`15` INT,
+`16` INT
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+0, 38021, 33882, 30103, 26627, 23408,
+20412, 17609
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+1, 37949, 33816, 30043, 26571, 23357,
+20364, 17564
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+2, 37877, 33750, 29983, 26516, 23305,
+20316, 17519
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+3, 37805, 33685, 29922, 26460, 23254,
+20267, 17474
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+4, 37733, 33619, 29862, 26405, 23202,
+20219, 17429
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+5, 37661, 33554, 29802, 26349, 23151,
+20171, 17384
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+6, 37589, 33489, 29743, 26394, 23099,
+20123, 17339
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+7, 37517, 33424, 29683, 26239, 23048,
+20075, 17294
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+8, 37446, 33359, 29623, 26184, 22997,
+20028, 17249
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+9, 37374, 33294, 29563, 26129, 22946,
+19980, 17204
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+10, 37303, 33229, 29504, 26074, 22894,
+19932, 17159
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+11, 37232, 33164, 29444, 26019, 22843,
+19884, 17114
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+12, 37161, 33099, 29385, 25964, 22792,
+19837, 17070
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+13, 37090, 33034, 29326, 25909, 22741,
+19789, 17025
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+14, 37019, 32970, 29267, 25854, 22690,
+19742, 16980
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+15, 36948, 32906, 29207, 25800, 22640,
+19694, 16936
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+16, 36878, 22842, 29148, 25745, 22589,
+19647, 16891
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+17, 36807, 32777, 29089, 25690, 22539,
+19599, 16847
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+18, 36737, 32713, 29031, 25636, 22488,
+19552, 16807
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+19, 36667, 32649, 28972, 25581, 22437,
+19504, 16758
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+20, 36597, 32585, 28913, 25527, 22386,
+19457, 16714
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+21, 36527, 32521, 28854, 25473, 22336,
+19410, 16669
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+22, 36457, 32458, 28796, 25419, 22286,
+19363, 16625
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+23, 36387, 32394, 28737, 25365, 22236,
+19316, 16581
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+24, 36318, 32331, 28679, 25311, 22185,
+19269, 16537
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+25, 36248, 32267, 28621, 25257, 22135,
+19222, 16493
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+26, 36179, 32204, 28562, 25203, 22084,
+19171, 16449
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+27, 36109, 32140, 28504, 25149, 22034,
+19128, 16405
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+28, 36040, 32077, 28446, 25095, 21984,
+19081, 16361
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+29, 35971, 32014, 28388, 25041, 21934,
+19034, 16317
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+30, 35902, 31951, 28330, 24988, 21884,
+18988, 16273
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+31, 35833, 31888, 28272, 24934, 21835,
+18941, 16229
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+32, 35765, 31826, 28214, 24881, 21785,
+18895, 16185
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+33, 35696, 31763, 28157, 24828, 21735,
+18848, 16141
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+34, 35627, 31700, 28099, 24774, 21685,
+18802, 16098
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+35, 35559, 31638, 28041, 24721, 21635,
+18755, 16054
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+36, 35491, 31575, 27984, 24667, 21586,
+18709, 16010
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+37, 35422, 31513, 27926, 24614, 21536,
+18662, 15967
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+38, 35354, 31451, 27869, 24561, 21487,
+18616, 15923
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+39, 35286, 31388, 27812, 24508, 21437,
+18569, 15880
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+40, 35218, 31326, 27755, 24455, 21388,
+18523, 15836
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+41, 35150, 31264, 27698, 24402, 21338,
+18477, 15793
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+42, 35083, 31203, 27641, 24349, 21289,
+18431, 15749
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+43, 35015, 31141, 27584, 24297, 21240,
+18385, 15706
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+44, 34948, 31079, 27527, 24244, 21191,
+18339, 15663
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+45, 34880, 31017, 24470, 24191, 21142,
+18293, 15620
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+46, 34813, 30956, 27413, 24138, 21093,
+18247, 15576
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+47, 34746, 30894, 27356, 24086, 21044,
+18201, 15533
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+48, 34679, 30833, 27300, 24033, 20995,
+18155, 15490
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+49, 34612, 30772, 27243, 23981, 20946,
+18110, 15447
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+50, 34545, 30710, 27187, 23928, 20897,
+18064, 15404
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+51, 34478, 30649, 27131, 23876, 20848,
+18018, 15361
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+52, 34411, 30588, 27075, 23824, 20800,
+17973, 15318
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+53, 34344, 30527, 27018, 23772, 20751,
+17927, 15275
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+54, 34278, 30466, 26962, 23720, 20702,
+17881, 15232
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+55, 34411, 30406, 26906, 23668, 20653,
+17836, 15190
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+56, 34146, 30345, 26850, 23616, 20605,
+17790, 15147
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+57, 34080, 30284, 26794, 23564, 20557,
+17745, 15104
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+58, 34014, 30224, 26738, 23512, 20509,
+17700, 15061
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+59, 33948, 30163, 26682, 23460, 20460,
+17654, 15019
+);
+/* INSERT QUERY */
+INSERT INTO LogaritmosTres(`minuto`, `10`,`11`, `12`, `13`, `14`, `15`, `16`)
+VALUES
+(
+60, 33882, 30103, 26627, 23408, 20412,
+17609, 14976
+);
