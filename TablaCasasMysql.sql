8686c1c46db7b3aecaaddd01da8d4f3c240e010c
julian777 <julian01cs@hotmail.com>
2017-06-29 00:17:04 -0600

    Tabla Casas 9°N

---
+++ TablaCasasMysql.sql
@@ -0,0 +1,292 @@
+CREATE TABLE TablaCasas(
+`SIDERAL_TIME` VARCHAR(100),
+`LATITUDE_ 9° N` VARCHAR(100)
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'H M S', '10 Acuario | 11 Piscis | 12 Aries | ASC Tauro | 2 Geminis | 3 Cancer |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'21 9 52', '| 15 | 16 | 20 | 22 54 | 21 | 17 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'21 13 50', '| 16 | 17 | 21 | 23 55 | 22 | 18 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'21 17 49', '| 17 | 18 | 22 | 24 56 | 23 | 19 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'21 21 46', '| 18 | 19 | 23 | 25 56 | 24 | 20 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'21 25 42', '| 19 | 20 | 24 | 26 56 | 25 | 21 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'21 29 38', '| 20 | 22 | 25 | 27 55 | 25 | 22 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'21 33 34', '| 21 | 23 | 26 | 28 54 | 26 | 23 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'21 37 28', '| 22 | 24 | 27 | 29 53 | 27 | 24 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'21 41 22', '| 23 | 25 | 29 | 0 Geminis 51 | 28 | 25 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'21 45 15', '| 24 | 26 | Tauro | 1 49 | 29 | 26 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'21 49 8', '| 25 | 27 | 1 | 2 47 | Cancer | 26 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'21 53 0', '| 26 | 28 | 2 | 3 44 | 1 | 27 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'21 56 51', '| 27 | 29 | 3 | 4 40 | 2 | 28 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'22 0 42', '| 28 | Aries | 4 | 5 37 | 3 | 29 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'22 4 32', '| 29 | 1 | 5 | 6 33 | 3 | Leo |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'22 8 22', '| Piscis | 2 | 6 | 7 29 | 4 | 1 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'22 12 11', '| 1 | 3 | 7 | 8 24 | 5 | 2 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'22 15 59', '| 2 | 4 | 8 | 9 19 | 6 | 3 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'22 19 47', '| 3 | 6 | 9 | 10 14 | 7 | 4 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'22 23 34', '| 4 | 7 | 10 | 11 9 | 8 | 5 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'22 27 21', '| 5 | 8 | 11 | 12 3 | 9 | 6 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'22 31 8', '| 6 | 9 | 12 | 12 57 | 10 | 6 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'22 34 53', '| 7 | 10 | 13 | 13 51 | 10 | 7 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'22 38 39', '| 8 | 11 | 14 | 14 44 | 11 | 8 | '
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'22 42 24', '| 9 | 12 | 15 | 15 38 | 12 | 9 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'22 46 8', '| 10 | 13 | 16 | 16 31 | 13 | 10 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'22 49 52', '| 11 | 14 | 17 | 17 23 | 14 | 11 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'22 53 36', '| 12 | 15 | 18 | 18 16 | 15 | 12 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'22 57 20', '| 13 | 16 | 19 | 19 8 | 16 | 13 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'23 1 3', '| 14 | 17 | 20 | 20 0 | 16 | 14 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'23 4 45', '| 15 | 18 | 21 | 20 52 | 17 | 15 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'23 8 28', '| 16 | 19 | 22 | 21 44 | 18 | 16 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'23 12 10', '| 17 | 20 | 22 | 22 36 | 19 | 16 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'23 15 52', '| 18 | 21 | 23 | 23 27 | 20 | 17 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'23 19 33', '| 19 | 22 | 24 | 24 19 | 21 | 18 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'23 23 15', '| 20 | 23 | 25 | 25 10 | 21 | 19 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'23 26 56', '| 21 | 24 | 26 | 26 1 | 22 | 20 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'23 30 37', '| 22 | 25 | 27 | 26 52 | 23 | 21 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'23 34 17', '| 23 | 26 | 28 | 27 43 | 24 | 22 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'23 37 58', '| 24 | 27 | 29 | 28 34 | 25 | 23 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'23 41 39', '| 25 | 28 | Geminis | 29 24 | 26 | 24 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'23 45 19', '| 26 | 29 | 1 | 0 Cancer 15 | 27 | 25 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'23 48 59', '| 27 | Tauro | 2 | 1 5 | 27 | 26 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'23 52 40', '| 28 | 1 | 3 | 1 56 | 28 | 27 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'23 56 20', '| 29 | 2 | 3 | 2 46 | 29 | 28 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'CASAS', '| 4 | 5 | 6 | 7 | 8 | 9 |'
+);
+/* INSERT QUERY */
+INSERT INTO TablaCasas(`SIDERAL_TIME`, `LATITUDE_ 9° N`)
+VALUES
+(
+'SIDERAL_TIME', 'LATITUDE 9° S'
+);
